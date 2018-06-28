8ae2d6961522ca3be7aceea3700b1fc9e4a9d197
szymonbaranczyk <szymon.baranczyk@gmail.com>
2018-06-28 23:40:27 +0200

    frontend - assigning lecturer to subject

---
+++ Web/src/app/subject-panel/subject-panel.component.ts
@@ -0,0 +1,47 @@
+import {Component, OnInit} from '@angular/core';
+import {Lecturer, LecturerService} from "../api/lecturer.service";
+import {Subject, SubjectService} from "../api/subject.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-subject-panel',
+  templateUrl: './subject-panel.component.html',
+  styleUrls: ['./subject-panel.component.css']
+})
+export class SubjectPanelComponent implements OnInit {
+
+  subjects: Subject[];
+  lecturers: Lecturer[];
+  form: FormGroup;
+
+  constructor(private lecturerService: LecturerService, private subjectService: SubjectService, private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.subjectService.getList().subscribe((list) => {
+      this.subjects = list;
+      this.prepareForm();
+    });
+    this.lecturerService.getList().subscribe((list) => {
+      this.lecturers = list;
+      this.prepareForm();
+    });
+  }
+
+  prepareForm() {
+    if (this.subjects && this.lecturers) {
+      this.form = this.fb.group({
+        lecturers: this.fb.array(
+          this.subjects.map(s => this.fb.control({value: null}))
+        )
+      })
+    }
+  }
+
+  submit() {
+    const lects: Lecturer[] = this.form.get('lecturers').value;
+    lects.forEach((l, i) =>
+      this.subjectService.assignLecturer(this.subjects[i].id, l.id))
+  }
+
+}
